25e7761d25ad9d25e2a98f30fc4cb5a6c2b689d7
Luiz Gustavo <coguima2@gmail.com>
2022-09-27 16:47:23 -0300

    Protejo finalizado

---
+++ 23.09.22/index.js
@@ -0,0 +1,15 @@
+var cliente = "Alcides Garcia";
+var conta = 67786734;
+var saldo = 322,95;
+console.log("cliente" + (cliente) + "\nconta " (conta) + \nsaldo +".");
+
+var cliente = "Alcides Garcia";
+var conta = 67768734;
+var saldo = 322.95;
+var deposito = 50.00;
+console.log("novo deposito na conta") + " \nsaldo atualizado " +(saldo+deposito)+".";
+
+var cliente = "Alcides Garcia";
+var conta = 67768734;
+var saldo = 322.95;
+console.log('cliente${conta} + \nconta ${conta} \nsaldo ${saldo}')
